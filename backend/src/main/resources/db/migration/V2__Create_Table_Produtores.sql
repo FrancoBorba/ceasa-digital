719828aababf7bf3cf17dc9b2b96e98f0ee233a0
Franco Borba <franco.borba14@gmail.com>
2025-09-16 15:00:52 -0300

    Adicionando Dockerfile e Docker Compose

--- backend/src/main/resources/db/migration/V2__Create_Table_Produtores.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE produtores (
-    usuario_id BIGINT PRIMARY KEY,
-    CONSTRAINT fk_produtor_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
-);
--- backend/src/main/resources/db/migration/V4__Create_Table_Entregadores.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE entregadores (
-    usuario_id BIGINT PRIMARY KEY,
-    cnt VARCHAR(255),                      -- Atributo 'cnt: string' [cite: 123]
-    CONSTRAINT fk_entregador_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
-);
