c96c8aec4189bc49b0714134faca590e41786400
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2025-09-17 08:29:22 -0300

    feat(backend): add the env config in backend

--- backend/src/main/java/br/com/uesb/ceasadigital/api/features/user/controller/UserController.java
+++ backend/src/main/java/br/com/uesb/ceasadigital/api/features/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.uesb.ceasadigital.api.features.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
-  @GetMapping()
-  public String helloWorld() {
-    return "Hello World";
-  }
+
+    @Value("${DEV_DATASOURCE_URL}")
+    private String databaseUrl;
+
+    @GetMapping()
+    public String helloWorld() {
+        return databaseUrl;
+    }
+
 }
