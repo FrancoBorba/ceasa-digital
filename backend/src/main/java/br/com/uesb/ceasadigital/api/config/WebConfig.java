e0203408e369e84baee87e5d800366e22343c330
HerculesDraycon <herculescarros2@gmail.com>
2025-10-13 16:21:34 -0300

    fix: add some imports and the correct path for package in WebConfig.java

--- backend/src/main/java/br/com/uesb/ceasadigital/api/config/WebConfig.java
+++ backend/src/main/java/br/com/uesb/ceasadigital/api/config/WebConfig.java
@@ -1,6 +1,10 @@
 //Classe que estou usando provisoriamente para nao ser bloqueado pelo backend sec.
-package main.java.br.com.uesb.ceasadigital.api.config;
+package br.com.uesb.ceasadigital.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
