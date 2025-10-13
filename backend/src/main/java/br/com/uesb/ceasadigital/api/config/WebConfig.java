e9bac729a67c7e570147221f5b1423ed6705f4d9
Hércules Sampaio <121320899+HerculesDraycon@users.noreply.github.com>
2025-10-12 23:06:11 -0300

    feat: Stablishes the first system fullstack communication by endpoints (#27)

---
+++ backend/src/main/java/br/com/uesb/ceasadigital/api/config/WebConfig.java
@@ -0,0 +1,32 @@
+//Classe que estou usando provisoriamente para nao ser bloqueado pelo backend sec.
+package main.java.br.com.uesb.ceasadigital.api.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/products/**").permitAll()
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Porta do Vite
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
