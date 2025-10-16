f4a30477b04947ab1012b0767cdf15e208bbb106
Hércules Sampaio <121320899+HerculesDraycon@users.noreply.github.com>
2025-10-16 09:35:55 -0300

    feat: Stablishes connection from home to the cart page, and the cart backend therefore (#39)

--- frontend/src/app/main.jsx
+++ frontend/src/app/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import React from "react";
 import "./index.css";
-import {RouterProvider } from "react-router";
+import {RouterProvider } from "react-router-dom";
 import { router } from "./router/routes.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </React.StrictMode>
 );
--- frontend/src/app/router/routes.jsx
+++ frontend/src/app/router/routes.jsx
@@ -1,5 +1,6 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Cart from "../pages/Cart/Cart";
 import LoginPage from "../pages/auth/LoginPage";
 import RegistrationPage from "../pages/auth/RegistrationPage";
 
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegistrationPage />,
-  }
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
 ]);
