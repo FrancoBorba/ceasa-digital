0c6eb152412c8865b787f2f13f517071d183761a
Caíque Santos Santana <caique@DESKTOP-4DLBN45.localdomain>
2025-11-23 23:18:49 -0300

    Adicionando as unidades de medidas conforme a lista enviada
    É possível adicionar 3 tipos de unidades:
    - Quilo(s): sendo kg, KG, Kg ou kG
    - Unidade(s): sendo un, UN, Un ou uN
    - Caixa(s): sendo cx, CX, Cx ou cX, controlados pela enum nova criada.

--- backend/src/main/java/br/com/uesb/ceasadigital/api/features/product/model/Product.java
+++ backend/src/main/java/br/com/uesb/ceasadigital/api/features/product/model/Product.java
@@ -8,8 +8,11 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import br.com.uesb.ceasadigital.api.features.categoria.model.Category;
+import br.com.uesb.ceasadigital.api.features.product.model.Enum.UnidadeMedida;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,54 +21,48 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_produtos")
 public class Product {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false , length = 255)
+  @Column(nullable = false, length = 255)
   private String nome;
 
-  @Column(nullable = false , precision =10 , scale = 2)
+  @Column(nullable = false, precision = 10, scale = 2)
   private BigDecimal preco;
 
-   @Column(name = "unidade_de_medida" ,nullable = false , length = 20)
-  private String unidadeDeMedida;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "unidade_de_medida", nullable = false, length = 20)
+  private UnidadeMedida unidadeDeMedida;
 
   @Column(columnDefinition = "TEXT")
   private String descricao;
 
   @Column(name = "foto_url")
-  private String fotoUrl;  
+  private String fotoUrl;
 
   @ManyToMany
-  @JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"),
-  inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+  @JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
   private Set<Category> categories;
 
   @CreationTimestamp // Define a data e a hora de criação
-  @Column(name = "criado_em",updatable = false) 
+  @Column(name = "criado_em", updatable = false)
   private LocalDateTime criadoEm;
 
   @UpdateTimestamp
   @Column(name = "atualizado_em")
   private LocalDateTime atualizadoEm;
 
+  // TODO: Relação Um-para-Muitos: Um Produto pode ter muitas Avaliações
 
+  // Relação Um-para-Muitos: Um Produto pode estar em muitos Itens de Pedido
 
-
-
-    // TODO: Relação Um-para-Muitos: Um Produto pode ter muitas Avaliações
-   
-
-    // Relação Um-para-Muitos: Um Produto pode estar em muitos Itens de Pedido
-
-    
-    // TODO: Mapear os outros relacionamentos (OfertaProdutor, EstoqueVirtual) quando as entidades existirem.
+  // TODO: Mapear os outros relacionamentos (OfertaProdutor, EstoqueVirtual)
+  // quando as entidades existirem.
 
   public Long getId() {
     return id;
@@ -91,11 +88,11 @@ public class Product {
     this.preco = preco;
   }
 
-  public String getUnidadeDeMedida() {
+  public UnidadeMedida getUnidadeDeMedida() {
     return unidadeDeMedida;
   }
 
-  public void setUnidadeDeMedida(String unidadeDeMedida) {
+  public void setUnidadeDeMedida(UnidadeMedida unidadeDeMedida) {
     this.unidadeDeMedida = unidadeDeMedida;
   }
 
@@ -107,7 +104,7 @@ public class Product {
     this.descricao = descricao;
   }
 
-    public String getFotoUrl() {
+  public String getFotoUrl() {
     return fotoUrl;
   }
 
