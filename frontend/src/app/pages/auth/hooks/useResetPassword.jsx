bc0290d9d8182f1b2314f9688642c9a13b788872
Arturgondim <arturgondim@DESKTOP-ILNV27B.localdomain>
2025-11-24 23:44:23 -0300

    paginas de senhas consertadas

--- frontend/src/app/pages/auth/hooks/useResetPassword.jsx
+++ frontend/src/app/pages/auth/hooks/useResetPassword.jsx
@@ -10,19 +10,29 @@ function useResetPassword() {
   const tryToResetPassword = async ({ token, password }) => {
     setIsLoading(true);
     setError(null);
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
+    if (!passwordRegex.test(password)) {
+       setError("A senha deve ter no mínimo 8 caracteres, letras maiúsculas, minúsculas, números e caracteres especiais.");
+       setIsLoading(false);
+       return;
+    }
 
     try {
-      await apiRequester.post("/auth/reset-password", {
+      await apiRequester.post("/users/reset-password", {
         token: token,
         password: password,
       });
 
       alert("Senha redefinida com sucesso!");
       navigate("/login");
+      
     } catch (err) {
       const status = err?.response?.status;
       if (status === 400) {
-        setError("Link de redefinição inválido ou expirado.");
+        setError("Link inválido, expirado ou senha não atende aos requisitos.");
+      } else if (status === 404) {
+        setError("Token não encontrado ou expirado.");
       } else {
         setError("Ocorreu um erro. Tente novamente.");
       }
